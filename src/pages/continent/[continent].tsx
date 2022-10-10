d4ea2fd1d34de3e7546c9375ee61739003f56bc3
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-10-10 01:10:12 -0300

    :sparkles: Atualização do README

--- src/pages/continent/[continent].tsx
+++ src/pages/continent/[continent].tsx
@@ -57,89 +57,84 @@ export default function Continent() {
   }
   return (
     <>
-      {Object.keys(dataContinentHooks).length === 0 
-      ? (
-      <Flex justify="center" align="center" h="100vh" border="1px solid red">
-        <Spinner color="#FFBA08"  thickness='4px'  size='xl'/>
-      </Flex>
-     ) 
-      
-      :
+      {Object.keys(dataContinentHooks).length === 0
+        ? (
+          <Flex justify="center" align="center" h="100vh" border="1px solid red">
+            <Spinner color="#FFBA08" thickness='4px' size='xl' />
+          </Flex>
+        )
 
-      (<Flex w="full" direction="column">
-      <Header isLoaded={isLoaded}>
-        <Link href="/">
-          <a>
-          <Skeleton isLoaded={isLoaded}>
-            <Icon
-              color="#000000"
-              as={RiArrowLeftSLine}
-              mr="auto"
-              w={8}
-              h={8}
-              lineHeight="1"
-              _hover={{
-                cursor: "pointer"
-              }}
-            />
-            </Skeleton>
-          </a>
-        </Link>
-      </Header>
-      <Box w="full">
-        <Skeleton display="flex" position="relative" w="full" maxH="500px" isLoaded={isLoaded}>
-          <Image filter="brightness(0.8)" w="full" objectFit="cover" maxH="500px" src={imageCityAleatory()} alt={dataContinentHooks?.name} bgColor="#000000" />
-          <Heading w="full" maxW={1160} display="flex" alignItems={{ base: "center", lg: "end" }} justifyContent={{ base: "center", lg: "start" }} mx="auto" position="absolute" right="0" left="0" h="full" color="gray.50">
-            <Text as="span" fontSize={{ base: "2xl", lg: "5xl" }} fontWeight="semibold" pb={{ base: `${0}`, lg: `${16}` }}>
-              {dataContinentHooks?.name}
-            </Text>
-          </Heading>
-        </Skeleton>
-      </Box>
-
-      <Grid as="section" w="full" maxW={1160} mx="auto" mb="10" textAlign="center">
-        <Flex flexDirection={{ base: "column", lg: "row" }} gap={{ base: "4", lg: "20" }} mt="20" alignItems="center" color="gray.700">
-          <SkeletonText w="full" noOfLines={6} isLoaded={isLoaded} maxW="600px" fontSize={{ base: "sm", lg: "2xl" }} px={{ base: '4', lg: "0" }} lineHeight="1.4" textAlign="justify">
-            <Box>
-              {dataContinentHooks?.text}
+        : (<Flex w="full" direction="column">
+            <Header isLoaded={isLoaded}>
+              <Link href="/">
+                <a>
+                  <Skeleton isLoaded={isLoaded}>
+                    <Icon
+                      color="#000000"
+                      as={RiArrowLeftSLine}
+                      mr="auto"
+                      w={8}
+                      h={8}
+                      lineHeight="1"
+                      _hover={{
+                        cursor: "pointer"
+                      }}
+                    />
+                  </Skeleton>
+                </a>
+              </Link>
+            </Header>
+            <Box w="full">
+              <Skeleton display="flex" position="relative" w="full" maxH="500px" isLoaded={isLoaded}>
+                <Image filter="brightness(0.8)" w="full" objectFit="cover" maxH="500px" src={imageCityAleatory()} alt={dataContinentHooks?.name} bgColor="#000000" />
+                <Heading w="full" maxW={1160} display="flex" alignItems={{ base: "center", lg: "end" }} justifyContent={{ base: "center", lg: "start" }} mx="auto" position="absolute" right="0" left="0" h="full" color="gray.50">
+                  <Text as="span" fontSize={{ base: "2xl", lg: "5xl" }} fontWeight="semibold" pb={{ base: `${0}`, lg: `${16}` }}>
+                    {dataContinentHooks?.name}
+                  </Text>
+                </Heading>
+              </Skeleton>
             </Box>
-          </SkeletonText>
-          <Stack direction="row" align="center" justify="center" spacing={{ base: "4", lg: "16" }} mx="auto">
-            <Info quant={dataContinentHooks?.countrinng} title="países" isLoaded={isLoaded} />
-            <Info quant={dataContinentHooks?.language} title="línguas" isLoaded={isLoaded} />
-            <Info quant={dataContinentHooks?.City?.length} title="cidades +100" isLoaded={isLoaded} />
-          </Stack>
-        </Flex>
 
-        <Skeleton color="gray.700" mt="20" mb="10" textAlign="initial" px={{ base: '4', lg: "0" }} isLoaded={isLoaded}>
-          <Heading display="inline">Cidades +100</Heading>
-        </Skeleton>
+            <Grid as="section" w="full" maxW={1160} mx="auto" mb="10" textAlign="center">
+              <Flex flexDirection={{ base: "column", lg: "row" }} gap={{ base: "4", lg: "20" }} mt="20" alignItems="center" color="gray.700">
+                <SkeletonText w="full" noOfLines={6} isLoaded={isLoaded} maxW="600px" fontSize={{ base: "sm", lg: "2xl" }} px={{ base: '4', lg: "0" }} lineHeight="1.4" textAlign="justify">
+                  <Box>
+                    {dataContinentHooks?.text}
+                  </Box>
+                </SkeletonText>
+                <Stack direction="row" align="center" justify="center" spacing={{ base: "4", lg: "16" }} mx="auto">
+                  <Info quant={dataContinentHooks?.countrinng} title="países" isLoaded={isLoaded} />
+                  <Info quant={dataContinentHooks?.language} title="línguas" isLoaded={isLoaded} />
+                  <Info quant={dataContinentHooks?.City?.length} title="cidades +100" isLoaded={isLoaded} />
+                </Stack>
+              </Flex>
 
-        <Flex textAlign={{ base: "center", lg: "initial" }} justifyContent={{ base: "center", lg: "initial" }} alignItems={{ base: "center", lg: "initial" }} flexDirection={{ base: "column", lg: "row" }} gap={42} m="0" wrap="wrap" >
+              <Skeleton color="gray.700" mt="20" mb="10" textAlign="initial" px={{ base: '4', lg: "0" }} isLoaded={isLoaded}>
+                <Heading display="inline">Cidades +100</Heading>
+              </Skeleton>
 
-          {dataContinentHooks?.City?.map((valueResponse: cityProps) => {
-            return (
+              <Flex textAlign={{ base: "center", lg: "initial" }} justifyContent={{ base: "center", lg: "initial" }} alignItems={{ base: "center", lg: "initial" }} flexDirection={{ base: "column", lg: "row" }} gap={42} m="0" wrap="wrap" >
 
-
-              <City
-                key={valueResponse.name}
-                imageUrl={valueResponse.banner}
-                name={valueResponse.name}
-                country={valueResponse.country}
-                flag={valueResponse.flag}
-                isLoaded={isLoaded}
-              />
+                {dataContinentHooks?.City?.map((valueResponse: cityProps) => {
+                  return (
 
 
-            )
-          })}
-        </Flex>
-      </Grid>
+                    <City
+                      key={valueResponse.name}
+                      imageUrl={valueResponse.banner}
+                      name={valueResponse.name}
+                      country={valueResponse.country}
+                      flag={valueResponse.flag}
+                      isLoaded={isLoaded}
+                    />
 
 
+                  )
+                })}
+              </Flex>
+            </Grid>
+        </Flex>)
 
-    </Flex>)
-      
       }
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,14 @@ import { Box, Flex, Grid, Heading, HStack, Image, Stack, Text } from "@chakra-ui
 import { Carousel } from "../components/Carousel";
 import { Header } from "../components/Header";
 import { TravelTypes } from "../components/TravelTypes";
-//import mirageJs from '../services/db';
-import { useEffect, useState } from 'react';
+
 
 
 export default function Home() {
-  return(
+  return (
     <>
-      <Header isLoaded={true}/>
-      <Flex 
+      <Header isLoaded={true} />
+      <Flex
         w="100%"
         bg="gray.700"
         h={335}
@@ -19,16 +18,15 @@ export default function Home() {
         bgPosition="center"
         bgRepeat="no-repeat"
         bgSize="cover"
-        
-      >
-        <Flex direction="column" h="100%" justify="center" ml={{base:"4" , lg:"32"}}>
-          <Heading maxW={426}  color="gray.100">5 Continentes, <Flex as="span" display="block">infinitas possibilidades</Flex></Heading>
-          <Text maxW={524} color="gray.200" mt="4" fontSize={{base: '16px', md: '18px', lg: '20px'}}>Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
-        </Flex>
-        <Image display={{base: "none", lg: "block"}} src="/Airplane.png" alt="Avião" mr="35" position="absolute" right={10} bottom={-10} />
-      </Flex>
 
-      <Flex  gap={{base:"12", lg:"32"}} wrap="wrap" maxW={{base: 266, lg:"full"}} justify="center" align="center" mx="auto" mt="32" pb={20} color="black" position="relative" _before={{
+      >
+        <Flex direction="column" h="100%" justify="center" ml={{ base: "4", lg: "32" }}>
+          <Heading maxW={426} color="gray.100">5 Continentes, <Flex as="span" display="block">infinitas possibilidades</Flex></Heading>
+          <Text maxW={524} color="gray.200" mt="4" fontSize={{ base: '16px', md: '18px', lg: '20px' }}>Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
+        </Flex>
+        <Image display={{ base: "none", lg: "block" }} src="/Airplane.png" alt="Avião" mr="35" position="absolute" right={10} bottom={-10} />
+      </Flex>
+      <Flex gap={{ base: "12", lg: "32" }} wrap="wrap" maxW={{ base: 266, lg: "full" }} justify="center" align="center" mx="auto" mt="32" pb={20} color="black" position="relative" _before={{
         content: `""`,
         position: "absolute",
         width: "90px",
@@ -36,26 +34,22 @@ export default function Home() {
         right: "0",
         left: "0",
         margin: "0 auto",
-        
+
         textAlign: "center",
         height: "1px",
         bg: "#000000"
       }}>
-        <TravelTypes text="vida nortuna" image="cocktail_1.png" alt="Terra"  />
-        <TravelTypes text="praia" image="surf_1.png" alt="Praia"  />
-        <TravelTypes text="moderno" image="building_1.png" alt="Moderno"  />
-        <TravelTypes text="clássico" image="museum_1.png" alt="Clássico"  />
-        <TravelTypes text="e mais..." image="earth.png" alt="e mais"  />
+        <TravelTypes text="vida nortuna" image="cocktail_1.png" alt="Terra" />
+        <TravelTypes text="praia" image="surf_1.png" alt="Praia" />
+        <TravelTypes text="moderno" image="building_1.png" alt="Moderno" />
+        <TravelTypes text="clássico" image="museum_1.png" alt="Clássico" />
+        <TravelTypes text="e mais..." image="earth.png" alt="e mais" />
       </Flex>
-
-      <Grid color='gray.700'  justifyContent="center" textAlign="center" mt={12} fontSize={{base:"xl", lg:"4xl"}}>
+      <Grid color='gray.700' justifyContent="center" textAlign="center" mt={12} fontSize={{ base: "xl", lg: "4xl" }}>
         <Text>Vamos nessa?</Text>
         <Text>Então escolha seu continente</Text>
       </Grid>
-
       <Carousel />
-      
-     
     </>
   )
 }
